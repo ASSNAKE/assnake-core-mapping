1c2fc3ef17ad34b899fb99ea193a422d2004ce66
Fedorov113 <fedorov.de@gmail.com>
2020-03-22 18:44:47 +0300

    transfer to new structure

--- assnake_core_mapping/snake_module_setup.py
+++ assnake_core_mapping/snake_module_setup.py
@@ -1,5 +1,5 @@
 import os
-from assnake.api.snake_module import SnakeModule
+import assnake
 from assnake.utils import read_yaml
 
 
@@ -7,7 +7,7 @@ from assnake_core_mapping.bwa.invocation_commands import map_bwa
 from assnake_core_mapping.bowtie2.invocation_commands import map_bowtie2
 
 this_dir = os.path.dirname(os.path.abspath(__file__))
-snake_module = SnakeModule(name = 'assnake-core-mapping', 
+snake_module = assnake.SnakeModule(name = 'assnake-core-mapping', 
                            install_dir = this_dir,
                            snakefiles = [
                                './bwa/workflow_fasta_from_db.smk', 
